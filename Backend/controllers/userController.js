c16b4d450af079c1b5e73cd891be05cac7cc11a8
laxmank2005 <laxman.karbhari@mit.asia>
2026-07-21 16:36:30 +0530

    second

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,20 +1,20 @@
-import { User } from "../models/userMOdel";
+import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
     try{
-        const { fullName, username, password, confirmPassworrd, gender}= req.body;
-        if (!fullName || !username || !password || !confirmPassworrd || !gender) {
+        const { fullName, username, password, confirmPassword, gender}= req.body;
+        if (!fullName || !username || !password || !confirmPassword || !gender) {
             return res.status(400).json({message:"All fields are required"});
     }
-    if (password !== confirmPassworrd) {
+    if (password !== confirmPassword) {
         return res.status(400).json({message:"Password and confirm password should be same"});
     }
     const user = await  User.findOne({username});
     if (user){
-        return res.status(400).jsson({message:"username already exist try different"});
+        return res.status(400).json({message:"username already exist try different"});
     }
-    const hashedPasword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     //Profile photo generation based on gender and username api
         const maleProfilePhoto = `https://avatarapi.runflare.run/public/boy=${username}`;
@@ -24,13 +24,20 @@ export const register = async (req, res) => {
         await User.create({
         fullName,
         username,
-        password:hashedPasword,
-        profilePhoto: gender === male ? maleProfilePhoto : femaleProfilePhoto,
+        password:hashedPassword,
+        profilePhoto: gender === "male" ? maleProfilePhoto : femaleProfilePhoto,
         gender
     })
+    return res.status(201).json({
+    message: "User registered successfully"
+});
+    
       
 }
     catch(error){
-        console.log( error);
+        console.error( error);
+        return res.status(500).json({
+        message: "Internal Server Error"
+    });
     }
 }
--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
-
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 8080;
 
 connectDB();
+
+// routes
+app.use("/api/v1/user", userRoutes);
+//http://localhost:8080/api/v1/user/register
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
